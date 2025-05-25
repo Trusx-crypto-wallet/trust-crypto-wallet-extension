bfbe99491d94a50e244c6ff6a8142df2fd455c90
Trusx-crypto-wallet <trustcryptowallet1978@gmail.com>
2025-05-25 18:05:11 +0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,9 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  extends: ['rainbow', 'plugin:prettier/recommended'],
-  root: true,
-  rules: {
-    'import/no-default-export': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'import/order': [
-      'warn',
-      {
-        alphabetize: {
-          order: 'asc',
-        },
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-        ],
-        'newlines-between': 'always',
-        pathGroups: [
-          {
-            group: 'internal',
-            pattern: '~/**',
-          },
-        ],
-      },
-    ],
-    'sort-imports': [
-      'warn',
-      {
-        ignoreCase: false,
-        ignoreDeclarationSort: true,
-        ignoreMemberSort: false,
-      },
-    ],
-    'prettier/prettier': ['warn', require('./.prettierrc.js')],
-  },
-};
+{
+  "extends": ["next/core-web-vitals"],
+  "rules": {
+    "@typescript-eslint/no-unused-vars": "warn",
+    "prettier/prettier": "warn",
+    "react/jsx-props-no-spreading": "off",
+    "import/no-unresolved": "off"
+  }
+}
