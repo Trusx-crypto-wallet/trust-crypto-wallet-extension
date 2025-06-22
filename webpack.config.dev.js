381e3ca3903b38396fdfddd67efd4b90c063dfb0
Trusx-crypto-wallet <trustcryptowallet1978@gmail.com>
2025-06-22 14:30:57 +0400

    fix: disable webpack-extension-reloader for webpack 5 compatibility
    - Comment out webpack-extension-reloader import causing ERESOLVE conflict
    - Temporarily disable extension hot reloading for webpack 5 compatibility
    - Resolve npm dependency tree conflict between webpack 5.94.0 and extension-reloader
    - Maintain all other development build functionality
    - Fix Render deployment build failures
    
    The extension reloader expected webpack ^4 but project uses 5.94.0, causing npm ERESOLVE errors. Hot reloading is disabled temporarily - manual refresh required during development until webpack 5 compatible alternative is implemented.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,13 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs');
 const { resolve } = require('path');
-
 const { EnvironmentPlugin } = require('webpack');
-const ExtensionReloader = require('webpack-extension-reloader');
-
+// Commented out problematic webpack extension reloader
+// const ExtensionReloader = require('webpack-extension-reloader');
 const manifest = require('./static/manifest.json');
 const config = require('./webpack.config');
-
 const basicPermissions = manifest.permissions;
 
 // Any key we want to override for dev builds we can do it here
@@ -17,13 +15,19 @@ const manifestOverride = {
 };
 
 const manifestFilePath = resolve(__dirname, './build/manifest.json');
-
 const manifestForBuild = require(manifestFilePath);
 const allowedPlugins = [];
 
 if (manifestForBuild.background.scripts) {
   console.log('building for firefox! disabling hot reload');
 } else {
+  // Temporarily disabled extension reloader for webpack 5 compatibility
+  console.log('Extension reloader disabled for webpack 5 compatibility');
+  
+  // TODO: Replace with webpack 5 compatible hot reload solution
+  // For now, manual refresh required during development
+  
+  /* Original code - commented out for webpack 5 compatibility:
   allowedPlugins.push(
     new ExtensionReloader({
       manifest: manifestFilePath,
@@ -35,6 +39,7 @@ if (manifestForBuild.background.scripts) {
       },
     }),
   );
+  */
 }
 
 module.exports = {
